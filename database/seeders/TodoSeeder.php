d100554155f308008fef065a9685786cf125ef09
Yu Odagaki <y-odagaki@three-in.com>
2021-12-22 12:03:59 +0900

    Seeder実装

--- database/seeders/TodoSeeder.php
+++ database/seeders/TodoSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Status;
+use App\Models\Todo;
 use Illuminate\Database\Seeder;
 
 class TodoSeeder extends Seeder
@@ -13,6 +15,22 @@ class TodoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // ステータスの登録
+        $doing = Status::create([
+            'name' => 'doing',
+        ]);
+
+        $done = Status::create([
+            'name' => 'done',
+        ]);
+
+        // TODOを2件作成
+        Todo::factory()->state([
+            'status_id' => $doing->id,
+        ])->create();
+
+        Todo::factory()->state([
+            'status_id' => $done->id,
+        ])->create();
     }
 }
