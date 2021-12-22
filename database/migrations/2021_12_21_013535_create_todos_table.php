c548658c79d22d6fb5a753d138945709bceaf4c4
Yu Odagaki <y-odagaki@three-in.com>
2021-12-22 10:04:23 +0900

    マイグレーション作成 - statuses - todos

---
+++ database/migrations/2021_12_21_013535_create_todos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->foreignId('status_id');
+            $table->foreign('status_id')->references('id')->on('statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
