5d9d7058aba287ff813b40a61e65fcba6407efec
Yu Odagaki <y-odagaki@three-in.com>
2021-12-22 11:03:32 +0900

    コントローラ実装 - 全件取得のみ

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -13,7 +14,9 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::all();
+
+        return $todos;
     }
 
     /**
